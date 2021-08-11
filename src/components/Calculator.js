a93c856f72bfd8f8adb8d520d9faf57286cad6f4
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-08-11 13:18:42 -0600

    Create logic files for Calculator and import them into Calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,6 +4,8 @@ import OtherOperations from './otherOperations';
 import Digits from './digits';
 import Operators from './operators';
 import Equal from './equal';
+import calculate from '../logic/calculate';
+import operate from '../logic/operate';
 
 export default class Calculator extends React.Component {
   constructor(props) {
