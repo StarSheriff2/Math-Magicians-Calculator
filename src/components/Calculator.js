107df4d3fee1619a43bd32194ea42d5f0c3cb6a6
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-08-11 14:08:42 -0600

    Create onClick listeners for num buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -7,10 +7,29 @@ import Equal from './equal';
 import calculate from '../logic/calculate';
 import operate from '../logic/operate';
 
+/**
+ * Given a button name and a calculator data object, return an updated
+ * calculator data object.
+ *
+ * Calculator data object contains:
+ *   total:s      the running total
+ *   next:String       the next number to be operated on with the total
+ *   operation:String  +, -, etc.
+ */
+
 export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    console.log(buttonName);
   }
 
   render() {
@@ -18,7 +37,7 @@ export default class Calculator extends React.Component {
       <div className="Calculator-container">
         <Display />
         <OtherOperations />
-        <Digits />
+        <Digits handleClick = { this.handleClick }/>
         <Operators />
         <Equal />
       </div>
