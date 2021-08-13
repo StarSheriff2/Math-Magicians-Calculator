af1d6e203a9157e297f7f56e9eb460e34c985670
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-08-13 16:27:32 -0600

    Refactor euqal.js

--- src/components/equal.js
+++ src/components/equal.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Equal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = {};
-  }
-
-  handleClick(e) {
-    const { handleClick } = this.props;
+const Equal = (props) => {
+  const handleClick = (e) => {
+    const { handleClick } = props;
     handleClick(e.target.value);
-  }
+  };
 
-  render() {
-    return (
-      <div className="Equal-container">
-        <button onClick={this.handleClick} type="button" className="Equal-btn btn" value="=">=</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Equal-container">
+      <button onClick={handleClick} type="button" className="Equal-btn btn" value="=">=</button>
+    </div>
+  );
+};
 
 Equal.propTypes = { handleClick: PropTypes.func };
 
 Equal.defaultProps = { handleClick: null };
+
+export default Equal;
